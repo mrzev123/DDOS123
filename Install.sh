357b42cba268e9b15ef006cb25285d07ac50a870
mrzev123 <39166589+mrzev123@users.noreply.github.com>
2020-05-14 02:44:59 -0700

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,27 @@
+
+echo " _   _ _  _    ____ _  _______ ____    ___ _____   ________  _   _ _____ 
+| | | | || |  / ___| |/ /___ /|  _ \  |_ _|_   _| |__  / _ \| \ | | ____|
+| |_| | || |_| |   | ' /  |_ \| |_) |  | |  | |     / / | | |  \| |  _|  
+|  _  |__   _| |___| . \ ___) |  _ <   | |  | |    / /| |_| | |\  | |___ 
+|_| |_|  |_|  \____|_|\_\____/|_| \_\ |___| |_|   /____\___/|_| \_|_____|
+                                                                         
+--------------------------Bangladesh Syber Pro--------------------------"
+echo "Start Prosess"
+sudo apt-get update && apt-get upgrade
+sudo cd
+sudo apt -y upgrade
+sudo python -V
+sudo python3 -V
+sudo apt update
+sudo apt install python-pip
+sudo pip2 --version
+sudo pip --version
+sudo apt update
+sudo pip3 --version
+sudo pip2 install --user awscli
+sudo pip3 install --user awscli
+sudo env | grep PATH
+pip2 show awscli
+echo "--Complite The Prosess--"
+echo "-----H4CK3R IT ZONE------"
+echo "ALL IS OK READY TO ATTACK"
---
+++ README.md
@@ -0,0 +1,35 @@
+======================================================
+
+H4CK3R IT ZONE 	ONLY PROVIDE BY EDUCATION PROPOSS ONLY
+
+======================================================
+
+At fast install pip3
+
+$ bash Install.sh
+
+
+
+
+
+
+----------------------
+$ cd Muster-Ddos
+$ chmod +x Muster-Ddos.py
+$ python3 Muster-Ddos.py
+
+------------------------------------------------------
+H4CK3R IT ZONE
+https://www.youtube.com/channel/UCcaCMB_QkshPcFpl-PgoNZg
+------------------------------------------------------
+if you learn ethicall hacking Subscribe our chanell and 
+join my facebook grup/page
+
+if you like this video pillage share to your friend sharing do not bad 
+if you share this video can learning to your friends
+
+My Facebook 
+my page https://web.facebook.com/Hackeritzonebangladeshsyberpro/?modal=admin_todo_tour
+my grup https://web.facebook.com/groups/1566066986878847/
+--------------------------------------------------------
+
